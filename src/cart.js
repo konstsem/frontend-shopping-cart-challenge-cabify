971c2e90b76ea3c9e2d05582dea7b3a07bb96c81
konstsem <konstantin.semenov.2@gmail.com>
2021-05-26 21:59:46 +0300

    fix: rm currency from price

--- src/cart.js
+++ src/cart.js
@@ -1,11 +1,11 @@
 const items = [
   {
-    productCode: 'Product code X7R2OPX', code: 'TSHIRT', name: 'T-Shirt', price: 20.00, currency: 'euro', imageName: 'shirt',
+    productCode: 'Product code X7R2OPX', code: 'TSHIRT', name: 'T-Shirt', price: 20.00, imageName: 'shirt',
   },
   {
-    productCode: 'Product code X2G2OPZ', code: 'MUG', name: 'Mug', price: 5.00, currency: 'euro', imageName: 'mug',
+    productCode: 'Product code X2G2OPZ', code: 'MUG', name: 'Mug', price: 5.00, imageName: 'mug',
   }, {
-    productCode: 'Product code X3W2OPY', code: 'CAP', name: 'Cap', price: 10.00, currency: 'euro', imageName: 'cap',
+    productCode: 'Product code X3W2OPY', code: 'CAP', name: 'Cap', price: 10.00, imageName: 'cap',
   },
 ];
 
